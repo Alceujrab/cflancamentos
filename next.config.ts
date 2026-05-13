16cef41e772cd3c3527d94b8d57900d44b7dbd4e
Alceujrab <alceujr.ab@gmail.com>
2026-05-13 20:25:18 -0300

    chore: add dummy webpack config to bypass turbopack if possible

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    return config;
+  },
 };
 
 export default nextConfig;
